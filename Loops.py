f60c0160ffdd9897c7b76bbb597577d58b6b9836
AabhashBogati <153408149+AabhashBogati@users.noreply.github.com>
2024-03-20 21:32:12 +1100

    Loops

---
+++ Loops.py
@@ -0,0 +1,14 @@
+for i in [0,1,2,3,4,5]:
+    print(i)
+
+for i in range(6):
+    print(i)
+
+names = ["Harry", "Hermione", "Ron", "Ginny", "Draco"]\
+
+for i in names:
+    print(i)
+
+name = "Harry"
+for char in name:
+    print(char)
